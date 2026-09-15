5587413d17ebba3ebaf70399d2fd5b1da2b6a261
Jagmohan <jagmohanyadav0077@gmail.com>
2026-09-16 01:05:06 +0530

    feat: add X-Response-Time latency header middleware in Express server

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,7 @@ app.use((req, res, next) => {
     const start = Date.now();
     res.on('finish', () => {
         const duration = Date.now() - start;
+        res.setHeader('X-Response-Time', `${duration}ms`);
         console.log(`[API] ${req.method} ${req.path} -> ${res.statusCode} (${duration}ms)`);
     });
     next();
