5375e7ca93a677df365fa258bb13d7cad26e6ccd
Jagmohan <jagmohanyadav0077@gmail.com>
2026-09-19 00:46:37 +0530

    db: add schema index for post owner reference field in Mongoose model

--- backend/models/posts.js
+++ backend/models/posts.js
@@ -29,5 +29,6 @@ const postSchema = new Schema({
 
 postSchema.index({ date: -1 });
 postSchema.index({ category: 1 });
+postSchema.index({ owner: 1 });
 
 module.exports = mongoose.model("Post", postSchema);
