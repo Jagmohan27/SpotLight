06744a000786553a09c63d82cfd5db0a29c9e02b
Jagmohan <jagmohanyadav0077@gmail.com>
2026-09-22 16:07:54 +0530

    sec: add X-Permitted-Cross-Domain-Policies security response header

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,7 @@ app.use((req, res, next) => {
     res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
     res.setHeader('X-DNS-Prefetch-Control', 'off');
     res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+    res.setHeader('X-Permitted-Cross-Domain-Policies', 'none');
     next();
 });
 
